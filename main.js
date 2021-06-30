7210a3c008c6ce4d4d0c83464cfd6aea44af5b84
DPlazonic <66380253+DPlazonic@users.noreply.github.com>
2021-06-30 20:38:11 +0200

    made db in js file

---
+++ main.js
@@ -0,0 +1,16 @@
+let db = [
+    {
+        name: "Danilo",
+        lastName: "Vesovic",
+        email: "danilo@gmail.com",
+        phone: "111-222-333",
+    },
+    {
+        name: "Ivan",
+        lastName: "Ivanov",
+        email: "ivan@gmail.com",
+        phone: "444-555-666",
+    }
+]
+
+let 
